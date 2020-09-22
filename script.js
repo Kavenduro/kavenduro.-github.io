027cec3f6e958deb956b34b3cb4fed9554163b60
Kavenduro <71184813+Kavenduro@users.noreply.github.com>
2020-09-22 09:22:30 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+ window.onload = function() {
+
+   Vue.component('eat', {
+      props: ['rol','col'],
+      template: '<div class="eat" :style="">aaa</div>'
+})
+
+
+   var field = new Vue ({
+       el: "#field"
+   })
+}
